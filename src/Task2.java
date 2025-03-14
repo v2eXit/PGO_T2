3bb4a20d96acc3f2b9f227e4ba3e74e374b992a7
s32572 <s32572@pjwstk.edu.pl>
2025-03-14 14:52:51 +0100

    Task 2

---
+++ src/Task2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Task2 {
+    public static int findDominant(int[] array) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        int threshold = array.length / 2;
+        for (int num : array) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+            if (countMap.get(num) > threshold) {
+                return num;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {3, 3, 3, 2, 2, 2, 2};
+        System.out.println(findDominant(arr)); // Output: 3
+    }
+}
