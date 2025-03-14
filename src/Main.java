a6ae8bb258be1762c9bebf45cc62c001ed47866f
s32572 <s32572@pjwstk.edu.pl>
2025-03-14 14:49:42 +0100

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static int[] filterEvenIndexOddValue(int[] array) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < array.length; i += 2) {
+            if (array[i] % 2 != 0) {
+                result.add(array[i]);
+            }
         }
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 5, 6, 7, 8};
+        System.out.println(Arrays.toString(filterEvenIndexOddValue(arr)));
     }
 }
